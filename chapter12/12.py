947fb8811b5e4c50d311240bf89eb0631920bc06
lvchenggang <lvchenggang@xyz.cn>
2016-05-28 15:10:07 +0800

    代码及文档

---
+++ chapter12/12.py
@@ -0,0 +1,8 @@
+import  os
+print(os.getcwd())
+
+print(os.listdir(os.curdir))
+
+print(os.path.join('bin', 'utils', 'disktools'))
+
+print(os.environ)
---
+++ chapter13/13.py
@@ -0,0 +1,3 @@
+file_object = open("myfile", 'r')
+
+file_object.close()
