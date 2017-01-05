709df33d71409dca8db39b7da3fba73ab59dd8a7
18965050 <18965050@qq.com>
2017-01-05 11:10:27 +0800

    代码及文档

--- chapter10/10.py
+++ chapter10/10.py
@@ -1,2 +1,9 @@
 import sys
 print(sys.path)
+
+from modtest import *
+print(f(3))
+
+from modtest import _g
+print(_g(3))
+
--- chapter13/13.py
+++ chapter13/13.py
@@ -1,3 +1,18 @@
 file_object = open("myfile", 'r')
 
 file_object.close()
+
+class A:
+    pass
+
+class B(A):
+    pass
+
+class C:
+    pass
+
+class D(B,C):
+    pass
+
+d=D()
+print(isinstance(d,B))
